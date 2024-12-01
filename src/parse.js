b4e079c9e04ef4914c7f3cd9228579102fc92e68
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2024-12-01 20:53:13 +0530

    constructed AST structure

--- src/parse.js
+++ src/parse.js
@@ -1,13 +1,40 @@
-const { pop } = require('./utils');
+const { isOpeningParenthesis, isClosingParenthesis } = require('./identify');
+const { pop, peek } = require('./utils');
 
-const parenthesize = () => {};
+const parenthesize = (tokens) => {
+    const token = pop(tokens);
+
+    if (isOpeningParenthesis(token.value)) {
+        const expression = [];
+
+        while (!isClosingParenthesis(peek(tokens).value)) {
+            expression.push(parenthesize(tokens));
+        }
+
+        pop(tokens);
+
+        return expression;
+    }
+
+    return token;
+};
 
 function parse(tokens) {
-    const token = pop(tokens);
+    if (Array.isArray(tokens)) {
+        const [first, ...rest] = tokens;
+
+        return {
+            type: 'CallExpression',
+            name: first.value,
+            arguments: rest.map(parse),
+        };
+    }
+
+    const token = tokens;
 
     if (token.type == 'Number') {
         return {
-            type: 'NumbericLiteral',
+            type: 'NumericLiteral',
             value: token.value,
         };
     }
@@ -27,6 +54,21 @@ function parse(tokens) {
     }
 }
 
+const tokens = [
+    { type: 'Parenthesis', value: '(' },
+    { type: 'Name', value: 'add' },
+    { type: 'Number', value: 2 },
+    { type: 'Number', value: 3 },
+    { type: 'Parenthesis', value: '(' },
+    { type: 'Name', value: 'add' },
+    { type: 'Number', value: 2 },
+    { type: 'Number', value: 3 },
+    { type: 'Parenthesis', value: ')' },
+    { type: 'Parenthesis', value: ')' },
+];
+
+// console.log(parse(parenthesize(tokens)));
+
 module.exports = {
-    parse,
+    parse: (tokens) => parse(parenthesize(tokens)),
 };
