96abf1ff94c7c2f933fdde8681788d419da3ec99
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2024-12-02 21:21:42 +0530

    added test to check the wheather it changes the value or not

--- tests/traverse.test.js
+++ tests/traverse.test.js
@@ -28,4 +28,33 @@ describe(traverse, () => {
 
         expect(ast.name).toBe('hello world');
     });
+
+    it('should traverse all the nodes in the ast tree and should double the number aka numeric literals', () => {
+        const ast = {
+            type: 'CallExpression',
+            name: 'add',
+            arguments: [
+                { type: 'NumericLiteral', value: 12 },
+                { type: 'NumericLiteral', value: 6 },
+            ],
+        };
+
+        const visitor = {
+            CallExpression: {
+                enter: (node) => {
+                    node.name = 'hello world';
+                },
+            },
+            NumericLiteral: {
+                exit: (node) => {
+                    node.value = node.value * 2;
+                },
+            },
+        };
+
+        traverse(ast, visitor);
+
+        expect(ast.arguments[0].value).toBe(24);
+        expect(ast.arguments[1].value).toBe(12);
+    });
 });
