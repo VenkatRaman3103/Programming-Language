47ba8431677e684dff2caa88cf21dd092dc3c143
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2024-12-03 21:50:16 +0530

    update

--- src/parse-and-evaluate.js
+++ src/parse-and-evaluate.js
@@ -1,8 +1,9 @@
 import { evaluate } from './evaluate.js';
 import { parseTokens } from './parse.js';
 import { tokenizer } from './tokenizer.js';
+import { transform } from './transform.js';
 import { pipe } from './utils.js';
 
-const parseAndEvaluate = pipe(tokenizer, parseTokens, evaluate);
+const parseAndEvaluate = pipe(tokenizer, parseTokens, transform, evaluate);
 
 export { parseAndEvaluate };
---
+++ src/transform.js
@@ -0,0 +1,3 @@
+export const transform = (node) => {
+    return node;
+};
