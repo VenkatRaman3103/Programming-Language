6bf24d708896e8c3ed61714383311a23ec77e472
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2024-12-05 22:09:36 +0530

    base logic for adding variables

--- src/transform.js
+++ src/transform.js
@@ -1,3 +1,45 @@
+import { traverse } from './traverse.js';
+
 export const transform = (node) => {
-    return node;
+    const visitor = {
+        CallExpression: {
+            enter(node) {
+                if (specialForm[node.name]) {
+                    specialForm[node.name](node);
+                }
+            },
+        },
+    };
+
+    return traverse(node, visitor);
 };
+
+const ast = {
+    type: 'CallExpression',
+    name: 'declare',
+    arguments: [
+        { type: 'NumericLiteral', value: 12 },
+        { type: 'NumericLiteral', value: 6 },
+    ],
+};
+
+const specialForm = {
+    declare(node) {
+        const identifier = node.arguments[0];
+        const assigment = node.arguments[1];
+
+        node.type = 'VariableDeclaration';
+
+        node.identifier = identifier;
+        node.assigment = assigment;
+
+        delete node.name;
+        delete node.arguments;
+    },
+};
+
+transform(ast);
+
+console.log(ast);
+
+export const specialFoms = {};
